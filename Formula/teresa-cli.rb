1221c3a7f19d9b000cb86ba32acd343d07953d79
Diego Garcia <drgarcia1986@gmail.com>
2017-11-08 16:30:57 -0200

    update to `v0.10.0`

--- Formula/teresa-cli.rb
+++ Formula/teresa-cli.rb
@@ -1,8 +1,8 @@
 class TeresaCli < Formula
   desc "Teresa client"
   homepage "https://github.com/luizalabs/teresa"
-  url "https://github.com/luizalabs/teresa/releases/download/v0.9.0/teresa-darwin-amd64"
-  sha256 "4461fd6c2ce6ee478119ad2fda08eb8ca001cbbe801006774fb343da51f31689"
+  url "https://github.com/luizalabs/teresa/releases/download/v0.10.0/teresa-darwin-amd64"
+  sha256 "cda28441bf35f80954f5640e9ba6a9c60a76024062511e3de8b611c773b58af9"
   head "https://github.com/luizalabs/teresa.git"
   version 'v0.9.0'
 
