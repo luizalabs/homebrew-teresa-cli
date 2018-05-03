72bcd3a8b5c970978b6548fb4044d2190ad322a4
Jair Henrique <jair.henrique@gmail.com>
2018-05-03 14:03:04 -0300

    Update teresa-cli.rb

--- Formula/teresa-cli.rb
+++ Formula/teresa-cli.rb
@@ -1,10 +1,10 @@
 class TeresaCli < Formula
   desc "Teresa client"
   homepage "https://github.com/luizalabs/teresa"
-  url "https://github.com/luizalabs/teresa/releases/download/v0.20.0/teresa-darwin-amd64"
-  sha256 "a2302dca23d36fabe3c42d646d015e8d49e319e0beba9b25f2adaa8d8a5c7b17"
+  url "https://github.com/luizalabs/teresa/releases/download/v0.21.0/teresa-darwin-amd64"
+  sha256 "588d201742e5ff9daa76290a39e7f4fb270543f246b513c1c5ba97eb2808e04e"
   head "https://github.com/luizalabs/teresa.git"
-  version 'v0.20.0'
+  version 'v0.21.0'
 
   bottle :unneeded
 
